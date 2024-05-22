0488f507e6e8c107b0fcc8114e576e8077122118
Luca-Rubino <lucasrubino92@gmail.com>
2024-05-22 23:47:04 +0200

    Creare gli oggetti con i dati richiesti

--- js/script.js
+++ js/script.js
@@ -1,1 +1,57 @@
 console.log('Js-our-team')
+
+// Traccia:
+// > Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+// > Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+// > MILESTONE 0:
+// - Creare l’array di oggetti con le informazioni fornite.
+// > MILESTONE 1:
+// - Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+// > MILESTONE 2:
+// - Stampare le stesse informazioni su DOM sottoforma di stringhe
+// > BONUS 1:
+// - Trasformare la stringa foto in una immagine effettiva
+// > BONUS 2:
+// - Organizzare i singoli membri in card/schede
+
+
+// Dati da inserire negli oggetti
+// Wayne Barnett	Founder & CEO	wayne-barnett-founder-ceo.jpg
+// Angela Caroll	Chief Editor	angela-caroll-chief-editor.jpg
+// Walter Gordon	Office Manager	walter-gordon-office-manager.jpg
+// Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+// Scott Estrada	Developer	scott-estrada-developer.jpg
+// Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg
+
+
+// creo gli oggetti con i dati richiesti
+const object1 = {
+    nome:'Wayne Barnett',
+    ruolo:'Founder & CEO',
+    foto:'img/wayne-barnett-founder-ceo.jpg',
+}
+const object2 = {
+    nome:'Angela Caroll',
+    ruolo:'Chief Editor',
+    foto:'img/angela-caroll-chief-editor.jpg',
+}
+const object3 = {
+    nome:'Walter Gordon',
+    ruolo:'Office Manager',
+    foto:'img/walter-gordon-office-manager.jpg',
+}
+const object4 = {
+    nome: 'Angela Lopez',
+    ruolo:'Social Media Manager',
+    foto:'img/angela-lopez-social-media-manager.jpg',
+}
+const object5 = {
+    nome: 'Scott Estrada',
+    ruolo:'Developer',
+    foto:'img/scott-estrada-developer.jpg',
+}
+const object6 = {
+    nome: 'Barbara Ramos',
+    ruolo:'Graphic Designer',
+    foto:'img/barbara-ramos-graphic-designer.jpg',
+}
